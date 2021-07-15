559f332a279bdbff023049be0fc5cb3127035944
Homa Zeighami <75029510+HomaZeighami@users.noreply.github.com>
2021-07-15 22:00:00 +0430

    Add files via upload

---
+++ Colored Terminal Text.py
@@ -0,0 +1,10 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.WHITE + Back.RED + Style.BRIGHT + "Hello, World!")
+print(Fore.BLACK + Back.CYAN + Style.DIM + "I'm Homa!")
+print(Fore.BLACK + Back.MAGENTA + Style.DIM + "I'm a tech nerd.")
+print(Fore.BLACK + Back.GREEN + Style.DIM + "I'm currently learning Python.")
+print(Fore.BLACK + Back.BLUE + Style.DIM + "Python is an awesome programming language!")
+print(Fore.BLACK + Back.YELLOW + Style.DIM + "There's just so much you can do with it.")
